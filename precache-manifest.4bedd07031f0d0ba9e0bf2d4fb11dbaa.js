4e61dd17b4d09815289c8b42a7f726acf92ec19f
srinichockalingam <55970187+srinichockalingam@users.noreply.github.com>
2019-11-07 23:54:28 +0800

    Updates

---
+++ precache-manifest.4bedd07031f0d0ba9e0bf2d4fb11dbaa.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aca89df12d6ecfa56747aa7fcd1664a",
+    "url": "/Demos/index.html"
+  },
+  {
+    "revision": "4ca0c732b371f1805ef8",
+    "url": "/Demos/static/css/main.0a9042b1.chunk.css"
+  },
+  {
+    "revision": "5f87c78016b37bc8c0e8",
+    "url": "/Demos/static/js/2.d4a1b7a2.chunk.js"
+  },
+  {
+    "revision": "4ca0c732b371f1805ef8",
+    "url": "/Demos/static/js/main.1386809b.chunk.js"
+  },
+  {
+    "revision": "bc2e9dd656d732a2e353",
+    "url": "/Demos/static/js/runtime-main.05b90466.js"
+  },
+  {
+    "revision": "10080290f7bacee6f7d36b1356b2aabb",
+    "url": "/Demos/static/media/Halcyon.10080290.jpg"
+  },
+  {
+    "revision": "2be738c525f05cc53cb918f89e25a09d",
+    "url": "/Demos/static/media/mdspcss.2be738c5.woff"
+  },
+  {
+    "revision": "58e4a392da9d6a9b83b0540130d910a6",
+    "url": "/Demos/static/media/mdspcss.58e4a392.eot"
+  },
+  {
+    "revision": "6d37302eb90f462b09769bcc27c952f0",
+    "url": "/Demos/static/media/mdspcss.6d37302e.woff2"
+  },
+  {
+    "revision": "ac8c9140e58960207eb95a29e4d38b4a",
+    "url": "/Demos/static/media/mdspcss.ac8c9140.ttf"
+  },
+  {
+    "revision": "62929671062ec761d3a48d6c0d19e02d",
+    "url": "/Demos/static/media/sisan03.62929671.woff"
+  },
+  {
+    "revision": "6a8dd6ebad01d15c483c476c40a04b84",
+    "url": "/Demos/static/media/sisan03.6a8dd6eb.woff2"
+  },
+  {
+    "revision": "9a5e121f4574a2c04c561a1b7736e6ac",
+    "url": "/Demos/static/media/sisan03.9a5e121f.eot"
+  },
+  {
+    "revision": "c17d6178bde063319df76699d7238bdf",
+    "url": "/Demos/static/media/sisan03.c17d6178.ttf"
+  },
+  {
+    "revision": "377b8df2c28b6e8c1dca03e0659ebe2b",
+    "url": "/Demos/static/media/sisan06.377b8df2.ttf"
+  },
+  {
+    "revision": "6fc13ae8c867bbc39985d8b7e0cd342e",
+    "url": "/Demos/static/media/sisan06.6fc13ae8.woff2"
+  },
+  {
+    "revision": "924a755d4cbcf5eb2b7478295db11a32",
+    "url": "/Demos/static/media/sisan06.924a755d.woff"
+  },
+  {
+    "revision": "f6058cbbd0483dc67e1262edbdc3c960",
+    "url": "/Demos/static/media/sisan06.f6058cbb.eot"
+  },
+  {
+    "revision": "48dc52482a29dba447547bd03582edcb",
+    "url": "/Demos/static/media/sisan08.48dc5248.woff2"
+  },
+  {
+    "revision": "5cf724c8ed8d7aa06455ae6d0340b231",
+    "url": "/Demos/static/media/sisan08.5cf724c8.ttf"
+  },
+  {
+    "revision": "81fc7cbbf22cefea428a2d4fe7370b1b",
+    "url": "/Demos/static/media/sisan08.81fc7cbb.woff"
+  },
+  {
+    "revision": "ab1e9e1da06a3c4fcaa11199dff3783e",
+    "url": "/Demos/static/media/sisan08.ab1e9e1d.eot"
+  },
+  {
+    "revision": "2d33db40a966470d2ffe44e879da6879",
+    "url": "/Demos/static/media/sisan33.2d33db40.eot"
+  },
+  {
+    "revision": "5e7257a4be6dc8ce6a94ef27a9529e4b",
+    "url": "/Demos/static/media/sisan33.5e7257a4.woff2"
+  },
+  {
+    "revision": "65b293f7bb10b569f50b3d3ee7b91d5c",
+    "url": "/Demos/static/media/sisan33.65b293f7.ttf"
+  },
+  {
+    "revision": "e48b349fce5d489c2e99925cfc8203e7",
+    "url": "/Demos/static/media/sisan33.e48b349f.woff"
+  },
+  {
+    "revision": "12c7df718e5e944f30218ea7c6f76626",
+    "url": "/Demos/static/media/sisan36.12c7df71.ttf"
+  },
+  {
+    "revision": "5f75c0a4f1446147dee5bb47c36090e5",
+    "url": "/Demos/static/media/sisan36.5f75c0a4.woff2"
+  },
+  {
+    "revision": "a1554405d4284dfa03bb6ef9e1a809b8",
+    "url": "/Demos/static/media/sisan36.a1554405.eot"
+  },
+  {
+    "revision": "c7cc9441063c1b47d512676cbd8c2876",
+    "url": "/Demos/static/media/sisan36.c7cc9441.woff"
+  },
+  {
+    "revision": "7b8eec6c014571bcde8d11b5a6431f35",
+    "url": "/Demos/static/media/sisan38.7b8eec6c.woff2"
+  },
+  {
+    "revision": "84c81f524f71cd0619d4cc7f6516cabb",
+    "url": "/Demos/static/media/sisan38.84c81f52.woff"
+  },
+  {
+    "revision": "8aad11ce0078df0349b1019059e1e3f4",
+    "url": "/Demos/static/media/sisan38.8aad11ce.eot"
+  },
+  {
+    "revision": "f7996b4d30e9a31c6f87b537b893b895",
+    "url": "/Demos/static/media/sisan38.f7996b4d.ttf"
+  }
+]);
